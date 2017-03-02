ed8d73435861c39386474fb6da75b62ee86d770c
KUBO Atsuhiro <kubo@iteman.jp>
2017-03-02 14:29:52 +0900

    fix spaces

--- src/EventListener/SessionConfigurationListener.php
+++ src/EventListener/SessionConfigurationListener.php
@@ -1,14 +1,14 @@
 <?php
 /*
  * Copyright (c) 2016 KUBO Atsuhiro <kubo@iteman.jp>,
-* All rights reserved.
-*
-* This file is part of PHPMentorsRouteBasedSessionConfigurationBundle.
-*
-* This program and the accompanying materials are made available under
-* the terms of the BSD 2-Clause License which accompanies this
-* distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
-*/
+ * All rights reserved.
+ *
+ * This file is part of PHPMentorsRouteBasedSessionConfigurationBundle.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
 
 namespace PHPMentors\RouteBasedSessionConfigurationBundle\EventListener;
 
